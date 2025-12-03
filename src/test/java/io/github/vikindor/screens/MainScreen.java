204625e18ef8f773aacae55b88e74325845db60f
Vikindor <vikindor@proton.me>
2025-12-03 17:18:10 +0300

    refactor: remove @Step annotations from SO

--- src/test/java/io/github/vikindor/screens/MainScreen.java
+++ src/test/java/io/github/vikindor/screens/MainScreen.java
@@ -10,20 +10,17 @@ public class MainScreen {
 
     SearchComponent searchComponent = new SearchComponent();
 
-    @Step("Enter text into the search bar on the main page")
     public MainScreen enterTextIntoSearchBar(String text) {
         searchComponent.searchContainer().click();
         searchComponent.searchSrcText().sendKeys(text);
         return this;
     }
 
-    @Step("Check that search results list is not empty")
     public MainScreen checkSearchResultIsNotEmpty() {
         searchComponent.pageListItemTitles().shouldHave(sizeGreaterThan(0));
         return this;
     }
 
-    @Step("Check that search results list contain exact title")
     public MainScreen checkSearchResultForTitle(String title) {
         searchComponent.pageListItemTitle().shouldHave(text(title));
         return this;
